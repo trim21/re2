1bfdd5b3d1ada42576b787e564ef97d957c70894
Trim21 <trim21.me@gmail.com>
2024-10-09 02:20:24 +0800

    add header

--- python/__init__.pyi
+++ python/__init__.pyi
@@ -1,3 +1,7 @@
+# Copyright 2024 The RE2 Authors.  All Rights Reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 from typing import (
     AnyStr,
     Callable,
